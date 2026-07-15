b2106d18542a739b0e374c70d0b205fb724c6a50
Subhraman Sarkar <sbmskmm@protonmail.com>
2026-07-15 06:51:17 +0530

    style(HelpWindow): remove space between func and (
    * add linebreak between system and project includes
    * add explanatory comment for src/Help.def

--- helpwindow.cpp
+++ helpwindow.cpp
@@ -1,12 +1,14 @@
 #include <QMainWindow>
 #include <QTableWidget>
 #include <QTableWidgetItem>
+
 #include "mainwindow.h"
 #include "helpwindow.h"
 
 #define help_def(ar, prim, name, title, descr) \
   {ar, prim, name, title, descr},
 
+// src/Help.def comes from gnu-apl source tree
 const help_s help[] = {
 #include "src/Help.def"
 };
@@ -18,25 +20,25 @@ int symbolsCount() {
 void
 HelpWindow::closeEvent(QCloseEvent *event __attribute__((unused)))
 {
-  mw->closeHW ();
+  mw->closeHW();
   delete this;
 }
 
-HelpWindow::HelpWindow (MainWindow *parent)
+HelpWindow::HelpWindow(MainWindow *parent)
   : QMainWindow(parent)
 {
   this->setWindowTitle ("qapl Help");
   mw = parent;
-  QWidget *hw = new QWidget ();
-  QVBoxLayout *layout = new QVBoxLayout;
+  QWidget* hw = new QWidget();
+  QVBoxLayout* layout = new QVBoxLayout;
   this->setCentralWidget(hw);
   
   const int rowCount = symbolsCount();
-  QTableWidget *table = new QTableWidget (rowCount, 2, this);
+  QTableWidget* table = new QTableWidget(rowCount, 2, this);
   table->setShowGrid(false);
   //  QStringList headers = {"Symbol", "Name", "Title", "Description"};
-  const QStringList headers = {"Name", "Title"};
-  table->setHorizontalHeaderLabels (headers);
+  const QStringList headers = {"Name", "Description"};
+  table->setHorizontalHeaderLabels(headers);
   
   for (int i = 0; i < rowCount; i++) {
     //  for (int i = 0; i < 10; i++) {
@@ -46,21 +48,21 @@ HelpWindow::HelpWindow (MainWindow *parent)
     table->setItem (i, 0, arityItem);
 #endif
 
-    QTableWidgetItem *primItem
-      = new QTableWidgetItem (QString (help[i].prim));
+    QTableWidgetItem* primItem
+      = new QTableWidgetItem(QString(help[i].prim));
 #if 1
-    table->setVerticalHeaderItem (i, primItem);
+    table->setVerticalHeaderItem(i, primItem);
 #else
     table->setItem (i, 0, primItem);
 #endif
     
-    QTableWidgetItem *nameItem
-      = new QTableWidgetItem (QString (help[i].name));
-    table->setItem (i, 0, nameItem);
+    QTableWidgetItem* nameItem
+      = new QTableWidgetItem(QString(help[i].name));
+    table->setItem(i, 0, nameItem);
     
-    QTableWidgetItem *titleItem
-      = new QTableWidgetItem (QString (help[i].title));
-    table->setItem (i, 1, titleItem);
+    QTableWidgetItem* titleItem
+      = new QTableWidgetItem(QString(help[i].title));
+    table->setItem(i, 1, titleItem);
 
 #if 0
     QTableWidgetItem *descItem
@@ -74,9 +76,8 @@ HelpWindow::HelpWindow (MainWindow *parent)
   table->setMinimumWidth (640);
   layout->addWidget(table);
 
-
   hw->setLayout(layout);
-  this->show ();
+  this->show();
 }
 
 HelpWindow::~HelpWindow()
